fe0ad9436107c7adc4eddb03f2d357d72981f79d
ffff <ffff@dfdsfsdf.sdfsdf>
2022-12-27 15:17:57 +0200

    refactoring ExportDataForm

--- src/Body/ExportDataForm.js
+++ src/Body/ExportDataForm.js
@@ -2,19 +2,20 @@ import { Button, Form } from "react-bootstrap";
 import { getForecast, getWeather } from "../Services/apiService";
 
 export function ExportDataForm() {
-  const dataFormats = ["JSON", "XML", "HTML"];
+  const dataFormats = ["json", "xml", "html"];
   const dataEndpoints = ["Current Weather Data", "5 day weather forecast"];
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const getDataEndpoint = e.target.endpoint.value;
     const getDataFormat = e.target.format.value;
 
     (async function () {
       const weather =
         getDataEndpoint === "Current Weather Data"
-          ? await getWeather(null, getDataFormat.toLowerCase())
-          : await getForecast(null, getDataFormat.toLowerCase());
+          ? await getWeather(null, getDataFormat)
+          : await getForecast(null, getDataFormat);
 
       const response = await weather.text();
 
@@ -23,7 +24,7 @@ export function ExportDataForm() {
         .document.body.append(
           (document.createElement(
             "p"
-          ).innerText = `${getDataEndpoint}, Data format: ${getDataFormat}`),
+          ).innerText = `${getDataEndpoint}, data format: ${getDataFormat}`),
           document.createElement("hr"),
           `${response}`
         );
@@ -31,6 +32,7 @@ export function ExportDataForm() {
   };
   return (
     <Form onSubmit={handleSubmit}>
+      <h5 className="my-4">Export</h5>
       <Form.Group className="my-4">
         <Form.Label>Choose current or 5 day forecast weather data:</Form.Label>
         {dataEndpoints.map((endpoint) => (
@@ -47,21 +49,17 @@ export function ExportDataForm() {
       </Form.Group>
       <Form.Group className="my-4">
         <Form.Label>Setting format data for export:</Form.Label>
-        {dataFormats.map((format) => (
-          <Form.Check
-            type="radio"
-            id={format}
-            label={format}
-            key={format}
-            name="format"
-            value={format}
-            defaultChecked={format === "JSON"}
-          />
-        ))}
+        <Form.Select name="format" defaultChecked="json">
+          {dataFormats.map((format) => (
+            <option key={format} value={format}>
+              {format}
+            </option>
+          ))}
+        </Form.Select>
       </Form.Group>
 
-      <Button className="w-100" variant="primary" type="submit">
-        Submit
+      <Button className="w-100" variant="warning" type="submit">
+        Export
       </Button>
     </Form>
   );
