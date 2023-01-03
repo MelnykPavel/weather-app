62e959076075d888c497ab00acc959e5c2889f61
MelnikPavel <pmelnik.mail@proton.me>
2023-01-03 17:35:54 +0200

    add

--- src/Body/Tabs/Forecast.js
+++ src/Body/Tabs/Forecast.js
@@ -1,20 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ErrorModal } from "../../ErrorModal";
 import { getForecast } from "../../Services/apiService";
 import { Map } from "./Map";
 import { TimeSelector } from "./TimeSelector";
 
 export function Forecast() {
+  const [errorMassage, setErrorMassage] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
   useEffect(() => {
     (async function () {
-      const weather = await getForecast();
-      const response = await weather.json();
-      console.log("response", response);
+      try {
+        const response = await getForecast();
+        const data = await response.json();
+        if (data.cod !== 200) {
+          throw Error(data.message);
+        }
+        setForecastData(data);
+      } catch (error) {
+        setErrorMassage(error.message);
+      }
     })();
   }, []);
+
   return (
     <>
-      <TimeSelector id={"forecast"} />
+      <TimeSelector data={forecastData} />
       <Map />
+      <ErrorModal
+        message={errorMassage}
+        handleClose={() => setErrorMassage(null)}
+      />
     </>
   );
 }
--- src/Body/Tabs/TimeSelector.js
+++ src/Body/Tabs/TimeSelector.js
@@ -1,36 +1,101 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Data } from "./Data";
+import moment from "moment";
 
-export function TimeSelector(props) {
-  const [radioValue, setRadioValue] = useState("1");
+export function TimeSelector({ data }) {
+  const [selectedDay, setSelectedDay] = useState(0);
+  const [selectedHour, setSelectedHour] = useState(0);
 
-  const radios = [
-    { name: "00:00", value: "1" },
-    { name: "03:00", value: "2" },
-    { name: "06:00", value: "3" },
-    { name: "09:00", value: "4" },
-    { name: "12:00", value: "5" },
-    { name: "15:00", value: "6" },
-    { name: "18:00", value: "7" },
-    { name: "21:00", value: "8" },
-  ];
+  const [days, setDays] = useState([]);
+  const [hours, setHours] = useState([]);
 
+  const [currentData, setCurrentData] = useState(null);
+
+  const getCurrentData = () => {
+    data?.list.foreach((item) => {
+      const timestamp = item.dt;
+      const momentDate = moment.unix(timestamp);
+
+      const day = momentDate.format("DD");
+      const hour = momentDate.format("HH:MM");
+
+      if (selectedDay === day && selectedHour === hour) {
+        setCurrentData(item);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const days = [];
+    const hours = [];
+
+    data?.list.foreach((item) => {
+      const timestamp = item.dt;
+      const momentDate = moment.unix(timestamp);
+
+      const day = momentDate.format("DD");
+      const hour = momentDate.format("HH:MM");
+
+      if (!days.includes(day)) {
+        days.push(day);
+      }
+      if (!hours.includes(hour)) {
+        hours.push(hour);
+      }
+    });
+    setDays(days);
+    setHours(hours);
+    setSelectedDay(days);
+    setSelectedHour(hours);
+    if (data) {
+      setCurrentData(data.list[0]);
+    }
+  }, [data]);
+
+  const handleOnChangeDays = (event) => {
+    setSelectedDay(event.currentTarget.value);
+    getCurrentData();
+  };
+  const handleOnChangeHours = (event) => {
+    setSelectedHour(event.currentTarget.value);
+    getCurrentData();
+  };
   return (
-    <ButtonGroup className="w-100">
-      {radios.map((radio, idx) => (
-        <ToggleButton
-          key={idx}
-          id={`${props.id}-${idx}`}
-          type="radio"
-          variant={"outline-primary"}
-          name={props.id}
-          value={radio.value}
-          checked={radioValue === radio.value}
-          onChange={(e) => setRadioValue(e.currentTarget.value)}
-        >
-          {radio.name}
-        </ToggleButton>
-      ))}
-    </ButtonGroup>
+    <>
+      <ButtonGroup className="w-100">
+        {days.map((day, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`day-${idx}`}
+            type="radio"
+            variant="outline-primary"
+            name="day"
+            value={day}
+            checked={selectedDay === day}
+            onChange={handleOnChangeDays}
+          >
+            {day}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+      <ButtonGroup className="w-100">
+        {hours.map((hour, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`hour-${idx}`}
+            type="radio"
+            variant={"outline-primary"}
+            name="hour"
+            value={hour}
+            checked={selectedHour === hour}
+            onChange={handleOnChangeHours}
+          >
+            {hour}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+      <Data data={currentData} />
+    </>
   );
 }
